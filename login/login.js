5d128cef54f385cd76184030acab60908c7fb3ce
Ahmed-dh41 <dahechahmed10@gmail.com>
2023-11-30 15:13:15 +0100

    topush

---
+++ login/login.js
@@ -0,0 +1,100 @@
+function each(array, func) { 
+    for (var i = 0; i < array.length; i++) { 
+          func(array[i], i); 
+    } 
+}
+
+function map(array, f) { 
+    var acc = []; 
+    each(array, function(element, i) { 
+          acc.push(f(element, i)); 
+    }); 
+    return acc; 
+}
+
+function filter(array, predicate) {
+ var acc = [];
+ each(array, function (element, index) {
+ 
+   if (predicate(element, index)) {
+     
+     acc.push(element);
+   }
+ });
+ return acc;
+}
+
+function reduce(array, f, acc) {
+ if (acc === undefined) {
+   acc = array[0];
+   array = array.slice(1);
+ }
+ each(array, function (element, i) {
+   acc = f(acc, element, i);
+ });
+ return acc;
+}
+var userData=[{
+    username:"ahmed",
+    password:"123456",
+    role:"user"
+},
+{
+    username:"yhya",
+    password:"654321",
+    role:"user"
+},
+{
+    username:"hechmi",
+    password:"147258369",
+    role:"user"
+},
+{   username:"hama",
+    password:"1920",
+    role:"user"
+
+
+
+}]
+function login(username, password,role) {
+    var user = userData.filter(function (user) {
+        return user.username === username && user.password === password && user.role===role
+    });
+
+    return user.length > 0 
+
+}
+
+$("#loginBtn").on("click", function () {
+    var username = $("#username").val()
+    var password = $("#password").val()
+    var role=$ ("#role").val()
+
+
+if (login(username, password,role)) {
+    window.location.replace("C:\Users\dahec\Desktop\project_paire\ahmed-yahya\user-interface\user.js");
+
+            // $('#loginStatus').text('Login successful!')
+            //  window.location.replace = "C:\Users\dahec\Desktop\project_paire\ahmed-yahya\user-interface\user.html"
+            }
+
+            // else if (login(username, password,role)) {
+            //     $('#loginStatus').text('Login successful!')
+            //     window.location.href = "admin.html"
+                
+            // }
+            else {
+                        alert('Invalid username or password  or role Please try again')
+                      
+                       
+                        $("#password").val("")
+                     }
+            
+                    })
+                    
+
+
+
+
+
+
---
+++ user-interface/user.js
@@ -0,0 +1,134 @@
+function each(array, func) { 
+    for (var i = 0; i < array.length; i++) { 
+          func(array[i], i); 
+    } 
+}
+
+function map(array, f) { 
+    var acc = []; 
+    each(array, function(element, i) { 
+          acc.push(f(element, i)); 
+    }); 
+    return acc; 
+}
+
+function filter(array, predicate) {
+ var acc = [];
+ each(array, function (element, index) {
+ 
+   if (predicate(element, index)) {
+     
+     acc.push(element);
+   }
+ });
+ return acc;
+}
+
+function reduce(array, f, acc) {
+ if (acc === undefined) {
+   acc = array[0];
+   array = array.slice(1);
+ }
+ each(array, function (element, i) {
+   acc = f(acc, element, i);
+ });
+ return acc;
+}
+var dummyData = [
+  {
+    name: "Phone",
+    image: "https://www.notebookcheck.net/typo3temp/_processed_/5/c/csm_4_to_3_Teaser_Telekom_T_Phone_5G_29111f3aca.jpg",
+    price: 29.99,
+    category: "Electronics",
+  },
+  {
+    name: "Jacket",
+    image: "https://www.stormtech.ca/cdn/shop/products/QX-1_FRONT_AzureBlue_b25b323a-4e71-4f4b-8c95-0083026df8a7_2000x.jpg?v=1687562447",
+    price: 49.99,
+    category: "Clothing",
+  },
+  {
+    name: "watch",
+    image: "https://tn.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/88/1246/1.jpg?4803  ",
+    price: 14.99,
+    category: "accsesori",
+  },
+  {
+    name: "ball",
+    image: "https://assets.adidas.com/images/w_383,h_383,f_auto,q_auto,fl_lossy,c_fill,g_auto/0f857c2b43d04735ab0faef500d99c04_9366/mls-pro-ball.jpg",
+    price: 39.99,
+    category: "Sports",
+  },
+  {
+    name: "Book",
+    image: "https://www.aibookgenerator.net/bookhome.png",
+    price: 9.99,
+    category: "Books",
+  },
+
+  {
+    name: "superman toy ",
+    image: "https://i5.walmartimages.com/seo/DC-Comics-12-Inch-Superman-Action-Figure-Kids-Toys-for-Boys_a4eda7a0-d50f-4f99-bfe6-0d187edf03bc.639b947874a63ceaa6e858835b72dc7c.jpeg?odnHeight=640&odnWidth=640&odnBg=FFFFFF",
+    price: 19.99,
+    category: "Toys",
+  },
+ 
+  {
+    name: "makeup",
+    image: "https://i.pinimg.com/736x/65/22/aa/6522aa1473fa1cc40eef482e1419fdf8.jpg",
+    price: 24.99,
+    category: "Beauty",
+  }
+  
+
+];
+
+
+
+
+
+
+function displayAllProduct() { 
+  $(".Product").empty();
+  dummyData.map(function(e) {
+    $(".Product").append(`
+     
+        <img src="${e.image}">
+        <h1>${e.name}</h1>
+        <h2>${e.price}</h2>
+        <h2>${e.category}</h2>
+        <button id="total">Add</button>
+      `
+    );
+  });
+}
+
+displayAllProduct();
+
+
+
+var totalprice=function(dummyData){
+  return reduce(dummyData,function(acc,e,i){
+      return acc+e.price
+  },0)
+}
+$('#total').on("click",totalprice)
+
+
+
+function searchProduct() {
+  var y = $("#input").val().toLowerCase();
+ 
+  var arr = [];
+
+  each(dummyData, function (e) {
+      if (e.name.toLowerCase() === y)
+
+          arr.push(e);
+         
+  }); 
+}
+
+
+
+
